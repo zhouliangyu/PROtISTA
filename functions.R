5f636a41b18baf34d22a8ad7d585d764f3d7accd
Zhouliang Yu <zhouliang@zhouliasmacbook.attlocal.net>
2018-09-04 20:43:39 -0700

    except the merged

--- functions.R
+++ functions.R
@@ -1,6 +1,16 @@
 # trim the last isoform character
 trim_iso <- function(id)
 {
-	return("")
+	splitted <- unlist(strsplit(id, "\\."))
+	mixed <- splitted[2]
+	result <- ""
+	for (i in 1:nchar(mixed))
+	{
+		currChar <-substr(mixed, i, i)
+		if (grepl("[0-9]", currChar))
+			result <- paste(result, currChar, sep="", collapse="")
+	}
+	result <- paste(splitted[1], ".", result, sep="", collapse="")
+	return(result)
 }
 
